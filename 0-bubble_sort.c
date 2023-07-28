5cd79fd9e1ce8ad7ca7e04b459213fb7627f58ad
Michafolab <topsonmich1@gmail.com>
2023-07-28 01:22:35 -0700

    adding a function that executes the Bubble-sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,11 @@
-#include "sort.h"
-#include <stdlib.h>
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include "sort.h"
 
 /**
- * bubble_sort - function that sorts an array
- * of integers in ascending order using the
- * Bubble sort algorithm
+ * main - Entry point
  *
- * @array: input arrray
- * @size: size of the array
- * Return: no return
+ * Return: Always 0
  */
 void bubble_sort(int *array, size_t size)
 {
